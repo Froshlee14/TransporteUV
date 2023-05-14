08039ec654886d5901d1a698bdfa5e0a72401e10
Froshle14 <kimkilwan135@gmail.com>
2023-05-14 09:29:05 -0600

    Modificaciones a creacion de tablas e implementacion de tablas restantes

--- src/main/webapp/sql/tablas.sql
+++ src/main/webapp/sql/tablas.sql
@@ -19,24 +19,32 @@ CREATE TABLE contactoEmergencia(
 	direccion varchar(120),
 	telefono varchar(10),
 	numEmpleado varchar(5),
-	CONSTRAINT pkNumEmpleado PRIMARY KEY(numEmpleado)
+	CONSTRAINT pkNumContacto PRIMARY KEY(numEmpleado),
 	FOREIGN KEY (numEmpleado) REFERENCES conductor(numEmpleado) ON DELETE CASCADE
-);
-
-CREATE TABLE fabricante(
-	idFabricante varchar(3),
-	nombreFabricante varchar(20),
-	CONSTRAINT pkIdFabricante PRIMARY KEY(idFabricante)
 );
 
 CREATE TABLE autobus(
 	numUnidad varchar(5) not null,
 	numSerie varchar(17),
-	idFabricante varchar(3),
+	idFabricante varchar(3) UNIQUE,
 	yearFabricacion int,
 	capacidad int,
 	CONSTRAINT pkNumUnidad PRIMARY KEY(numUnidad)
-	FOREIGN KEY (idFabricante) REFERENCES fabricante(idFabricante) ON DELETE CASCADE
+);
+
+CREATE TABLE conductorAutobus(
+	numEmpleado varchar(5),
+	numUnidad varchar(5),
+	fecha date,
+	FOREIGN KEY (numEmpleado) REFERENCES conductor(numEmpleado) ON DELETE CASCADE,
+	FOREIGN KEY (numUnidad) REFERENCES autobus(numUnidad) ON DELETE CASCADE
+);
+
+CREATE TABLE fabricante(
+	idFabricante varchar(3),
+	nombreFabricante varchar(20),
+	CONSTRAINT pkIdFabricante PRIMARY KEY(idFabricante),
+	FOREIGN KEY (idFabricante) REFERENCES autobus(idFabricante) ON DELETE CASCADE
 );
 
 CREATE TABLE ruta(
@@ -49,9 +57,32 @@ CREATE TABLE ruta(
 
 CREATE TABLE viaje(
 	numViaje varchar(5) not null,
-	diraccion varchar(100),
+	direccion varchar(100),
 	horaPartida TIME not null,
 	horaLlegada TIME not null,
 	CONSTRAINT pkNumViaje PRIMARY KEY(numViaje)
 );
 
+CREATE TABLE autobusViaje(
+	numUnidad varchar(5),
+	numViaje varchar(5),
+	FOREIGN KEY (numUnidad) REFERENCES autobus(numUnidad) ON DELETE CASCADE,
+	FOREIGN KEY (numViaje) REFERENCES viaje(numViaje) ON DELETE CASCADE
+);
+
+CREATE TABLE autobusRuta(
+	numRuta varchar(5),
+	numUnidad varchar(5),
+	FOREIGN KEY (numRuta) REFERENCES ruta(numRuta) ON DELETE CASCADE,
+	FOREIGN KEY (numUnidad) REFERENCES autobus(numUnidad) ON DELETE CASCADE
+);
+
+--DROP TABLE conductor;
+--DROP TABLE contactoEmergencia;
+--DROP TABLE autobus;
+--DROP TABLE conductorAutobus;
+--DROP TABLE fabricante;
+--DROP TABLE ruta;
+--DROP TABLE viaje;
+--DROP TABLE autobusViaje;
+--DROP TABLE autobusRuta;
