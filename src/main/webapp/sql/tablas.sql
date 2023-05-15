26c0dfc376933093c4887493a5b0cbb3b8d84566
Froshle14 <kimkilwan135@gmail.com>
2023-05-14 18:45:46 -0600

    Correcion de errores en tablas e inserciones.

--- src/main/webapp/sql/tablas.sql
+++ src/main/webapp/sql/tablas.sql
@@ -1,5 +1,5 @@
 CREATE TABLE conductor(
-	numEmpleado varchar(5) not null,
+	numEmpleado SERIAL,
 	nombre varchar(30),
 	apellidoPaterno varchar(30),
 	apellidoMaterno varchar(30),
@@ -8,81 +8,85 @@ CREATE TABLE conductor(
 	direccion varchar(120),
 	telefono varchar(10),
 	yearsExp int,
-	CONSTRAINT pkNumEmpleado PRIMARY KEY(numEmpleado)
+	PRIMARY KEY(numEmpleado)
 );
 
 CREATE TABLE contactoEmergencia(
-	numContacto varchar(5) not null,
+	numContacto SERIAL,
 	nombre varchar(30),
 	apellidoPaterno varchar(30),
 	apellidoMaterno varchar(30),
 	direccion varchar(120),
 	telefono varchar(10),
-	numEmpleado varchar(5),
-	CONSTRAINT pkNumContacto PRIMARY KEY(numEmpleado),
+	numEmpleado int,
+	PRIMARY KEY(numEmpleado),
 	FOREIGN KEY (numEmpleado) REFERENCES conductor(numEmpleado) ON DELETE CASCADE
 );
 
+CREATE TABLE fabricante(
+	idFabricante SERIAL,
+	nombreFabricante varchar(20),
+	PRIMARY KEY(idFabricante)
+);
+
 CREATE TABLE autobus(
-	numUnidad varchar(5) not null,
+	numUnidad SERIAL,
 	numSerie varchar(17),
-	idFabricante varchar(3) UNIQUE,
+	idFabricante int,
 	yearFabricacion int,
 	capacidad int,
-	CONSTRAINT pkNumUnidad PRIMARY KEY(numUnidad)
+	PRIMARY KEY(numUnidad),
+	FOREIGN KEY (idFabricante) REFERENCES fabricante(idFabricante)
 );
 
 CREATE TABLE conductorAutobus(
-	numEmpleado varchar(5),
-	numUnidad varchar(5),
-	fecha date,
+	numEmpleado int,
+	numUnidad int,
 	FOREIGN KEY (numEmpleado) REFERENCES conductor(numEmpleado) ON DELETE CASCADE,
 	FOREIGN KEY (numUnidad) REFERENCES autobus(numUnidad) ON DELETE CASCADE
 );
 
-CREATE TABLE fabricante(
-	idFabricante varchar(3),
-	nombreFabricante varchar(20),
-	CONSTRAINT pkIdFabricante PRIMARY KEY(idFabricante),
-	FOREIGN KEY (idFabricante) REFERENCES autobus(idFabricante) ON DELETE CASCADE
-);
-
 CREATE TABLE ruta(
-	numRuta varchar(5) not null,
+	numRuta SERIAL,
 	descripcion varchar(100),
 	destinoInicial varchar(50),
 	destinoFinal varchar(50),
-	CONSTRAINT pkNumRuta PRIMARY KEY(numRuta)
+	PRIMARY KEY(numRuta)
 );
 
 CREATE TABLE viaje(
-	numViaje varchar(5) not null,
+	numViaje SERIAL,
 	direccion varchar(100),
 	horaPartida TIME not null,
 	horaLlegada TIME not null,
-	CONSTRAINT pkNumViaje PRIMARY KEY(numViaje)
+	PRIMARY KEY(numViaje)
 );
 
 CREATE TABLE autobusViaje(
-	numUnidad varchar(5),
-	numViaje varchar(5),
+	numUnidad int,
+	numViaje int,
 	FOREIGN KEY (numUnidad) REFERENCES autobus(numUnidad) ON DELETE CASCADE,
 	FOREIGN KEY (numViaje) REFERENCES viaje(numViaje) ON DELETE CASCADE
 );
 
 CREATE TABLE autobusRuta(
-	numRuta varchar(5),
-	numUnidad varchar(5),
-	FOREIGN KEY (numRuta) REFERENCES ruta(numRuta) ON DELETE CASCADE,
-	FOREIGN KEY (numUnidad) REFERENCES autobus(numUnidad) ON DELETE CASCADE
+	numUnidad int,
+	numRuta int,
+	FOREIGN KEY (numUnidad) REFERENCES autobus(numUnidad) ON DELETE CASCADE,
+	FOREIGN KEY (numRuta) REFERENCES ruta(numRuta) ON DELETE CASCADE
 );
 
---DROP TABLE conductor;
---DROP TABLE contactoEmergencia;
---DROP TABLE autobus;
---DROP TABLE conductorAutobus;
---DROP TABLE fabricante;
---DROP TABLE ruta;
---DROP TABLE viaje;
---DROP TABLE autobusViaje;
---DROP TABLE autobusRuta;
+--En caso de ser necesario
+--se borra de forma descendente
+--por las llaves foraneas
+/*
+DROP TABLE autobusRuta;
+DROP TABLE autobusViaje;
+DROP TABLE viaje;
+DROP TABLE ruta;
+DROP TABLE fabricante;
+DROP TABLE conductorAutobus;
+DROP TABLE autobus;
+DROP TABLE contactoEmergencia;
+DROP TABLE conductor;
+*/
