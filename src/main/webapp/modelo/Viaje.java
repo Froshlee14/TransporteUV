29f4d9bb9a32c5234d6a3e376c5ce0e293e5aa0a
Froshle14 <kimkilwan135@gmail.com>
2023-05-15 12:07:59 -0600

    Adicion del JB de Viaje.

---
+++ src/main/webapp/modelo/Viaje.java
@@ -0,0 +1,60 @@
+import java.io.Serializable;
+
+public class Viaje implements Serilizable {
+	
+	private int numViaje;
+	private String direccion;
+	private String horaPartida;
+	private String horaLlegada;
+	
+	public Viaje() {
+		//Constructor vacio
+	}
+	
+	public Viaje(
+				int numViaje,
+				String direccion,
+				Time horaPartida,
+				Time horaLlegada
+			) {
+		
+		this.numViaje = numViaje;
+		this.direccion = direccion;
+		this.horaPartida = horaPartida;
+		this.horaLlegada = horaLlegada;
+		
+	}
+	
+	public int getNumViaje() {
+		return numViaje;
+	}
+	
+	public void setNumViaje(int num) {
+		this.numViaje = num;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String dir) {
+		this.direccion = dir;
+	}
+	
+	public String getHoraPartida() {
+		return horaPartida;
+	}
+	
+	public void setHoraPartida(String hor) {
+		this.horaPartida = hor;
+	}
+	
+	public String getHoraLlegada() {
+		return horaLlegada;
+	}
+	
+	public void setHoraLlegada(String hor) {
+		this.horaLlegada = hor;
+	}
+	
+}
