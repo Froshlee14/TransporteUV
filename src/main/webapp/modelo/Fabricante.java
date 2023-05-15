8a379664b43bd30c457200182166a85027866e55
Froshle14 <kimkilwan135@gmail.com>
2023-05-14 21:30:35 -0600

    Implementacion del JB de Fabricante.

---
+++ src/main/webapp/modelo/Fabricante.java
@@ -0,0 +1,34 @@
+import java.io.Serializable;
+
+public class Fabricante implements Serializable {
+	
+	private int idFabricante;
+	private String nombreFabricante;
+	
+	public Fabricante() {
+		//Constructor vacio
+	}
+	
+	public Fabricante(int idFabricante,String nombreFabricante) {
+		this.idFabricante = idFabricante;
+		this.nombreFabricante = nombreFabricante;
+	}
+
+    public int getIdFabricante() {
+        return idFabricante;
+    }
+    
+    public void setIdFabricante(int id) {
+        this.idFabricante = id;
+    }
+
+    public String getNombreFabricante() {
+        return nombreFabricante;
+    }
+    
+    public void setNombreFabricante(String nomf) {
+        this.nombreFabricante = nomf;
+    }
+
+	
+}
