eea076e9220f5dc512cf035895b979a521545f59
Froshle14 <kimkilwan135@gmail.com>
2023-06-01 02:14:59 -0600

    Servlet para login (aun no funciona)

---
+++ src/main/java/controlador/ServletLogin.java
@@ -0,0 +1,55 @@
+package controlador;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import javax.servlet.*;
+
+import modelo.Usuario;
+import datos.UsuarioDAO;
+
+@WebServlet("/ServletLogin")
+
+public class ServletLogin extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+      	UsuarioDAO userdao = new UsuarioDAO();
+      	String user = request.getParameter("usuario");
+      	String password = request.getParameter("passwrd").trim();
+      	
+        Usuario usuario = userdao.mostrar(user);
+        
+        if (usuario!=null) {
+        	if (usuario.getPasswrd().equals(password)) {
+        		
+        		 request.setAttribute("usuario",usuario);
+                
+                if (usuario.getRol().equals("admin")) {
+                    // Redirigir a la página indexAdmin.jsp
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("indexAdmin.jsp");
+                    dispatcher.forward(request, response);
+                }
+                else if (usuario.getRol().equals("empleado")) {
+                    // Redirigir a la página indexEmpleado.jsp
+                    RequestDispatcher dispatcher = request.getRequestDispatcher("indexEmpleado.jsp");
+                    dispatcher.forward(request, response);
+                }
+                else {
+                    // El rol no coincide con ningún valor esperado
+                } 
+                
+        	}
+        	
+        }
+        else {
+        	//No se encontro al usuario en la base de datos
+        }
+            
+        
+
+    }
+
+}
