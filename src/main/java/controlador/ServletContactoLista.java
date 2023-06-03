d0d51c88e610f64232c627f8f39f07fe5f427ac1
Froshle14 <kimkilwan135@gmail.com>
2023-06-02 21:14:42 -0600

    servel para buscar contactos de emergencia

---
+++ src/main/java/controlador/ServletContactoLista.java
@@ -0,0 +1,39 @@
+package controlador;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import java.io.*;
+import javax.servlet.*;
+import java.util.*;
+
+import modelo.ContactoEmergencia;
+import datos.ContactoEmergenciaDAO;
+import datos.ConductorDAO;
+
+@WebServlet("/ServletContactoLista")
+public class ServletContactoLista extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+      	ContactoEmergenciaDAO condao = new ContactoEmergenciaDAO();
+      	
+      	int numEmpleado = Integer.parseInt(request.getParameter("inputNumEmpleado"));
+      	
+        List<ContactoEmergencia> lista = condao.selecionar(numEmpleado);
+        
+      	ConductorDAO conductordao = new ConductorDAO();
+    	String nomEmpleado = conductordao.seleccionarNombre(numEmpleado);
+    	System.out.println(nomEmpleado);
+    	request.setAttribute("nombreEmpleado",nomEmpleado);
+            
+        request.setAttribute("lista",lista);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("listaContactos.jsp");
+        dispatcher.forward(request, response);
+    }
+
+
+}
