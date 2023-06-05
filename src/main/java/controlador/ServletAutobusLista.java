3003abe32ef7d5f575bd18e80acbdf72d86136d6
Froshle14 <kimkilwan135@gmail.com>
2023-06-05 10:24:12 -0600

    Implementacion de servlet para enlistar autobuses de la BD

---
+++ src/main/java/controlador/ServletAutobusLista.java
@@ -0,0 +1,34 @@
+package controlador;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import javax.servlet.*;
+import java.util.*;
+
+import modelo.Autobus;
+import datos.AutobusDAO;
+
+@WebServlet("/ServletAutobusLista")
+
+public class ServletAutobusLista extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+		AutobusDAO adao = new AutobusDAO();
+        List<Autobus> lista = adao.selecionarJoin();
+        if (lista.isEmpty()) {
+          	System.out.println("Lista vacia ");
+        }
+        else {
+        	System.out.println("Aqui hay datos ");
+        }
+            
+        request.setAttribute("lista",lista);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("autobusLista.jsp");
+        dispatcher.forward(request, response);
+    }
+
+}
