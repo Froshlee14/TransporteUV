fb2b7c2d4bf1760930d9316ae529a73617436824
Froshle14 <kimkilwan135@gmail.com>
2023-06-05 00:32:19 -0600

    Nuevo constructor

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -8,32 +8,21 @@ public class Usuario implements Serializable{
 	private String passwrd;
 	private int numEmpleado;
 	private String rol;
-	private boolean status;
 	
 	public Usuario() {
 	}
 	
-	public Usuario(String usuario, String passwrd, int numEmpleado, String rol, boolean status) {
+	public Usuario(String usuario, String passwrd, int numEmpleado, String rol) {
 		this.usuario = usuario;
 		this.passwrd = passwrd;
 		this.numEmpleado = numEmpleado;
 		this.rol = rol;
-		this.status = status;
 	}
 
-	public Usuario(String passwrd, int numEmpleado, String rol,boolean status) {
+	public Usuario(String passwrd, int numEmpleado, String rol) {
 		this.passwrd = passwrd;
 		this.numEmpleado = numEmpleado;
 		this.rol = rol;
-		this.status = status;
-	}
-
-	public boolean getStatus() {
-		return status;
-	}
-
-	public void setStatus(boolean status) {
-		this.status = status;
 	}
 
 	public String getUsuario() {
