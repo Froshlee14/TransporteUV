d91e9b31bedfa05982d708dca858fd44cf671b72
Froshle14 <kimkilwan135@gmail.com>
2023-06-09 17:10:11 -0600

    Servlet que precarga los datos del empleado al formulario para actualizacion de registro

---
+++ src/main/java/controlador/ServletConductorBuscarEmpleado.java
@@ -0,0 +1,39 @@
+package controlador;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.*;
+import javax.servlet.*;
+
+import modelo.Conductor;
+import datos.ConductorDAO;
+
+import modelo.Usuario;
+import datos.UsuarioDAO;
+
+@WebServlet("/ServletConductorBuscarEmpleado")
+
+public class ServletConductorBuscarEmpleado extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		
+		Conductor conductor = new Conductor();
+      	ConductorDAO condao = new ConductorDAO();
+      	
+		Usuario usuario = new Usuario();
+      	UsuarioDAO udao = new UsuarioDAO();
+      	
+      	int numEmpleado = Integer.parseInt(request.getParameter(("numEmpleado")));
+      	
+        conductor = condao.buscar(numEmpleado);
+        usuario = udao.buscarPorEmpleado(numEmpleado);
+		
+        request.setAttribute("conductor", conductor);
+        request.setAttribute("usuario", usuario);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("conductorModificarEmpleado.jsp");
+        dispatcher.forward(request, response);
+     
+	}
+}
