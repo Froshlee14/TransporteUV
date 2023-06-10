d35865321f706fb7d94a33c99fc77b17d1c3d598
Froshle14 <kimkilwan135@gmail.com>
2023-06-09 21:58:09 -0600

    Nueva funcion para enlistar viajes

--- src/main/java/datos/ViajeDAO.java
+++ src/main/java/datos/ViajeDAO.java
@@ -19,6 +19,15 @@ public class ViajeDAO {
 			+ "JOIN conductorAutobus ON autobus.numUnidad = conductorAutobus.numUnidad\r\n"
 			+ "JOIN conductor ON conductorAutobus.numEmpleado = conductor.numEmpleado\r\n"
 			+ "WHERE conductor.numEmpleado=?;";
+	
+	public static final String selectExtendidoSQL = "SELECT viaje.numViaje, viaje.direccion, viaje.horaPartida, viaje.horaLlegada,ruta.descripcion as ruta, autobus.numUnidad, (conductor.nombre||' '||conductor.apellidoPaterno||' '||conductor.apellidoMaterno) as conductor\r\n"
+			+ "	FROM viaje JOIN rutaViaje ON viaje.numViaje = rutaViaje.numViaje \r\n"
+			+ "	JOIN ruta ON rutaViaje.numRuta = ruta.numRuta \r\n"
+			+ "	JOIN autobusRuta ON ruta.numRuta = autobusRuta.numRuta\r\n"
+			+ "	JOIN autobus ON autobusRuta.numUnidad = autobus.numUnidad\r\n"
+			+ "	JOIN conductorAutobus ON autobus.numUnidad = conductorAutobus.numUnidad\r\n"
+			+ "	JOIN conductor ON conductorAutobus.numEmpleado = conductor.numEmpleado\r\n"
+			+ "	ORDER BY viaje.horaPartida ASC;";
 
 	public static final String selectNoAsignadosSQL = "SELECT viaje.numViaje, viaje.direccion, viaje.horaPartida, viaje.horaLlegada FROM viaje LEFT JOIN rutaViaje ON viaje.numViaje = rutaViaje.numViaje WHERE rutaViaje.numViaje IS NULL;";
 	public static final String selectBuscaSQL = "SELECT * FROM viaje WHERE numViaje=?";
@@ -156,6 +165,42 @@ public class ViajeDAO {
 				
 				viaje = new Viaje(numViaje,direccion,horaPartida,horaLlegada);
 				viajes.add(viaje);
+			}
+			Conexion.close(result);
+			Conexion.close(state);
+			Conexion.close(conn);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return viajes;
+	}
+	
+	public List<Viaje> selecionarExtendido(){
+        Connection conn = null;
+        Statement state = null;
+        ResultSet result = null;
+        Viaje ru = null;
+		
+		List<Viaje> viajes = new ArrayList<>();
+		
+		try {
+            conn = Conexion.getConnection();
+            state = conn.createStatement();
+            result = state.executeQuery(selectExtendidoSQL);
+			
+			while(result.next()) {
+				int numViaje = result.getInt("numViaje");
+				String direccion = result.getString("direccion");
+				Time horaPartida = result.getTime("horaPartida");
+				Time horaLlegada = result.getTime("horaLlegada");
+				String ruta =  result.getString("ruta");
+				int numUnidad = result.getInt("numUnidad");
+				String conductor = result.getString("conductor");
+				
+				ru = new Viaje(numViaje,direccion,horaPartida,horaLlegada,ruta,numUnidad,conductor);
+				viajes.add(ru);
 			}
 			Conexion.close(result);
 			Conexion.close(state);
